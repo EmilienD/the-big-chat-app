4e28f9eb0abe96787f911531124959aece2cc901
Durieu Emilien <contact@emilienduri.eu>
2021-06-22 22:30:22 +0200

    feat: add belgian football emotes

--- src/components/organisms/Emote.js
+++ src/components/organisms/Emote.js
@@ -5,6 +5,21 @@ export const emotes = {
   dovy: {
     mimetype: 'png',
   },
+  bobmoeilijk: {
+    mimetype: 'png',
+  },
+  frankoh: {
+    mimetype: 'png',
+  },
+  frankworstelen: {
+    mimetype: 'png',
+  },
+  hazardburger: {
+    mimetype: 'png',
+  },
+  theking: {
+    mimetype: 'png',
+  },
 }
 
 const Emote = ({ name }) => {
