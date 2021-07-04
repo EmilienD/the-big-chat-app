225246d0f43a58575c71da77b7470328669daa68
Durieu Emilien <contact@emilienduri.eu>
2021-07-04 14:05:18 +0200

    chore: add env file example, captain-definition and ignore build artifacts

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@
 export const conf = {
   reactAppIncomingDataUrl: process.env.REACT_APP_INCOMING_DATA_URL,
   reactAppOutgoingDataUrl: process.env.REACT_APP_OUTGOING_DATA_URL,
+  reactAppUserServiceUrl: process.env.REACT_APP_USER_SERVICE_URL,
 }
